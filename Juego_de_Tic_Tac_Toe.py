f9a3e161a89e378d973dee2a1404cd58c591bcbb
zoe <zoe.rebolledo@gmail.com>
2023-09-14 18:14:43 -0600

    actualización de color y tamaño

--- Juego_de_Tic_Tac_Toe.py
+++ Juego_de_Tic_Tac_Toe.py
@@ -16,28 +16,33 @@ from freegames import line
 def grid():
     """Draw tic-tac-toe grid."""
     line(-67, 200, -67, -200)
-    line(67, 200, 67, -200)
+    line(67, 100, 67, -200)
     line(-200, -67, 200, -67)
     line(-200, 67, 200, 67)
 
 
 def drawx(x, y):
     """Draw X player."""
-    line(x, y, x + 133, y + 133)
-    line(x, y + 133, x + 133, y)
+    pencolor("red")
+    line(x, y, x + 100, y + 100)
+    line(x, y + 100, x + 100, y)
 
 
 def drawo(x, y):
     """Draw O player."""
+    pencolor("blue")
     up()
     goto(x + 67, y + 5)
     down()
-    circle(62)
+    circle(50)
 
 
 def floor(value):
     """Round value down to grid with square size 133."""
     return ((value + 200) // 133) * 133 - 200
+
+
+tablero = [['' for _ in range(3)] for _ in range(3)]
 
 
 state = {'player': 0}
@@ -46,13 +51,18 @@ players = [drawx, drawo]
 
 def tap(x, y):
     """Draw X or O in tapped square."""
+
     x = floor(x)
     y = floor(y)
     player = state['player']
-    draw = players[player]
-    draw(x, y)
-    update()
-    state['player'] = not player
+    if tablero[y][x] == '':
+        draw = players[player]
+        draw(x, y)
+        update()
+        state['player'] = not player
+        tablero[y][x] = 'X' if player == 0 else 'O'
+    else:
+        print("Esta casilla ya está ocupada. Elige otra.")
 
 
 setup(420, 420, 370, 0)
